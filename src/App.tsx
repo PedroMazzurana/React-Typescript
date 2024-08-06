ce9ab573fb1ee8cab7d033c1eae783242b23fa77
Pedro Mazzurana <mazzurana@yahoo.com>
2024-08-06 19:41:49 -0300

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Botao from './componentes/Botao'
+
+function App() {
+  return (
+    <div className="App">
+      <Botao />
+    </div>
+  );
+}
+
+export default App;
